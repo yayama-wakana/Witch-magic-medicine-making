7aa577e6f9762386e7c8e5c9a2ffb483cba5d8ec
yayama-wakana <22016047@hiroshima.kosen-ac.jp>
2020-06-20 15:24:09 +0900

    プレイヤー落下時（画面下に消える）のゲームオーバーを実装

--- www/sketch.js
+++ www/sketch.js
@@ -1,3 +1,12 @@
+/**
+ * @preserve Credits
+ * 
+ * "p5.js でゲーム制作" ( https://fal-works.github.io/make-games-with-p5js/ )
+ * Copyright (c) 2020 FAL
+ * Used under the MIT License
+ * ( https://fal-works.github.io/make-games-with-p5js/docs/license/ )
+ */
+
 let titel, home, mix, collect, pbook, herb_pbook, med_pbook;
 var flag = 2;
 
@@ -25,12 +34,7 @@ function setup(){
 
   if(flag == 2){
     rectMode(CENTER);
-
-  // プレイヤーを作成
-  player = createPlayer();
-
-  // ブロックを作成
-  blocks = [];
+    resetGame();
   }
 }
 
@@ -96,8 +100,7 @@ function mousePressed(){
   }
 */
   if(flag == 2){
-    // プレイヤーをジャンプさせる
-    applyJump(player);
+    onMousePress();
   }
 }
 
@@ -148,6 +151,12 @@ function mousePressed(){
     rect(entity.x, entity.y, 80, 400);
   }
 
+  function playerIsAlive(entity) {
+    // プレイヤーの位置が生存圏内なら true を返す。
+    // 600 は画面の下端
+    return entity.y < 800;
+  }
+
   function blockIsAlive(entity) {
     // ブロックの位置が生存圏内なら true を返す。
     // -100 は適当な値（ブロックが見えなくなる位置であればよい）
@@ -160,6 +169,9 @@ function mousePressed(){
   /** ブロックエンティティの配列 */
   let blocks;
 
+  /** ゲームの状態。"play" か "gameover" を入れるものとする */
+  let gameState;
+
   /** ブロックを上下ペアで作成し、`blocks` に追加する */
   function addBlockPair() {
     let y = random(-100, 100);
@@ -167,12 +179,38 @@ function mousePressed(){
     blocks.push(createBlock(y + 600)); // 下のブロック
   }
 
-  // ゲームdraw関数
-  function collect_game(){
-  
+  /** ゲームオーバー画面を表示する */
+  function drawGameoverScreen() {
+    background(0, 192); // 透明度 192 の黒
+    fill(255);
+    textSize(64);
+    textAlign(CENTER, CENTER); // 横に中央揃え ＆ 縦にも中央揃え
+    text("GAME OVER", width / 2, height / 2); // 画面中央にテキスト表示
+  }
+
+  /** ゲームの初期化・リセット */
+  function resetGame() {
+    // 状態をリセット
+    gameState = "play";
+
+     // プレイヤーを作成
+    player = createPlayer();
+
+    // ブロックを作成
+    blocks = [];
+  }
+
+  /** ゲームの更新 */
+  function updateGame() {
+  // ゲームオーバーなら更新しない
+    if (gameState === "gameover") 
+      return;
+
   // ブロックの追加と削除
-  if (frameCount % 120 === 1) addBlockPair(blocks); // 一定間隔で追加
-  blocks = blocks.filter(blockIsAlive); // 生きているブロックだけ残す
+    if (frameCount % 120 === 1) 
+      addBlockPair(blocks); // 一定間隔で追加
+
+    blocks = blocks.filter(blockIsAlive); // 生きているブロックだけ残す
 
   // 全エンティティの位置を更新
     updatePosition(player);
@@ -182,9 +220,43 @@ function mousePressed(){
   // プレイヤーに重力を適用
     applyGravity(player);
 
+  // プレイヤーが死んでいたらゲームオーバー
+    if (!playerIsAlive(player)) 
+      gameState = "gameover";
+  }
+
+  /** ゲームの描画 */
+  function drawGame() {
   // 全エンティティを描画
     background(0);
     drawPlayer(player);
     for(let block of blocks)
       drawBlock(block);
+
+   // ゲームオーバーならそれ用の画面を表示
+    if (gameState === "gameover") 
+      drawGameoverScreen();
+  }
+
+  /** マウスボタンが押されたときのゲームへの影響 */
+  function onMousePress() {
+    // プレイヤーをジャンプさせる
+    applyJump(player);
+
+      switch (gameState) {
+        case "play":
+        // プレイ中の状態ならプレイヤーをジャンプさせる
+          applyJump(player);
+          break;
+        case "gameover":
+       // ゲームオーバー状態ならリセット
+          resetGame();
+          break;
+      }
+  }
+
+  // ゲームdraw関数
+  function collect_game(){
+    updateGame();
+    drawGame();   
   }
